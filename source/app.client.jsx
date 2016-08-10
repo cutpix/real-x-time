1a1010b2c7ed04ca250e2ef1610fe2eaa668f60a
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-10 06:36:40 +0300

    add redux provider to pass store to react container component

--- source/app.client.jsx
+++ source/app.client.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
 
+import configureStore from './store/config.store';
 import 'css/styles.css';
 import routes from './routes';
 
+const store = configureStore();
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>,
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('root')
 );
