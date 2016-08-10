4f1aa45f59ead909665c30a715ae27d31be92491
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-10 06:13:30 +0300

    add store / configure / install redux-immutable-state-invariant

---
+++ source/store/config.store.js
@@ -0,0 +1,13 @@
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+// good way to initialize your store with some state 
+// a specialy then doing server side rendering
+export default function configureStore(initialState) {
+    return createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(reduxImmutableStateInvariant())
+    );
+}
