d11c5875e2da06ea0796122d12a5b4d47ffeb6dd
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-11 05:35:13 +0300

    Enable Webpack hot module replacement for reducers

--- source/store/configure.store.js
+++ source/store/configure.store.js
@@ -5,9 +5,19 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 // good way to initialize your store with some state 
 // a specialy then doing server side rendering
 export default function configureStore(initialState) {
-    return createStore(
+    const store = createStore(
         rootReducer,
         initialState,
         applyMiddleware(reduxImmutableStateInvariant())
     );
+
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers/root.reducer', () => {
+            const nextRootReducer = require('../reducers/root.reducer').default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
+    return store;
 }
