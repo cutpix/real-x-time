cf27060b34717b5f05f45ef90907a5f8be983ed3
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-14 02:55:55 +0300

    add action types using for clarity in reduced

--- source/reducers/course.reducer.js
+++ source/reducers/course.reducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/action.types';
+
 export default function courseReducer(state = [], action) {
     switch (action.type) {
-        case 'CREATE_COURSE':
+        case types.CREATE_COURSE:
             // state.push(action.course);
             // return state;
             // create new array with extra value using spread operator (...)
