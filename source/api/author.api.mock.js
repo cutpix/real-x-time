6705de21ca05426efb022de1a9a9d89f81ff70f8
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-15 13:36:39 +0300

    prepair api use and add mock for it

---
+++ source/api/author.api.mock.js
@@ -0,0 +1,35 @@
+import delay from './delay';
+
+const authors = [
+    {
+        id: 'andrew-pavlyuk',
+        firstName: 'Andrew',
+        lastName: 'Pavlyuk'
+    },
+    {
+        id: 'alex-savchuk',
+        firstName: 'Alex',
+        lastName: 'Savchuk'
+    },
+    {
+        id: 'vladimir-olifer',
+        firstName: 'Vladimir',
+        lastName: 'Olifer'
+    }
+];
+
+class AuthorApi {
+    static getAllAuthors() {
+
+    }
+
+    static saveAuthor(author) {
+
+    }
+
+    static deleteAuthor(authorId) {
+
+    }
+}
+
+export default AuthorApi;
