299c6c5cf6a084c599318a5d14f5be5702dde111
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-17 23:01:08 +0300

    create load corse thunk

--- source/actions/course.actions.js
+++ source/actions/course.actions.js
@@ -1,5 +1,13 @@
 import * as types from './action.types';
+import CourseApi from '../api/course.api.mock';
 
 export function createCourse(course) {
     return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourses() {
+    // thunk always return a func that accept dispatch
+    return function (dispatch) {
+        return CourseApi.getAllCourses();
+    };
 }
