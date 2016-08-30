752a73a7cc075eeb5bdb672ec161090ec93ff8e2
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-30 20:23:20 +0300

    edit course form with common inputs

--- source/components/course/course.form.jsx
+++ source/components/course/course.form.jsx
@@ -2,20 +2,37 @@ import React from 'react';
 import TextInput from '../common/text.input';
 import SelectInput from '../common/select.input';
 
-const CourseForm = (props) => {
+const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
     return (
         <form>
             <h1>Manage Course Form</h1>
-            <TextInput />
-            <SelectInput />
-            <TextInput />
+            <TextInput
+                name="title"
+                label="Title"
+                value={course.title}
+                onChange={onChange}
+                error={errors.title} />
+            <SelectInput
+                name="authorId"
+                label="Author"
+                value={course.authorId}
+                defaultOption="Select Author"
+                options={allAuthors}
+                onChange={onChange}
+                error={errors.authorId} />
+            <TextInput
+                name=""
+                label=""
+                value={course.category}
+                onChange={onChange}
+                error={errors.category} />
             <TextInput />
             <input
                 type="submit"
-                disabled={props.loading}
-                value={props.loading ? 'Saving...' : 'Save'}
+                disabled={loading}
+                value={loading ? 'Saving...' : 'Save'}
                 className="btn btn-primary"
-                onClick={props.onSave} />
+                onClick={onSave} />
         </form>
     );
 };
