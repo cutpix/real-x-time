9851e59dd208f675dc815d5e7ad23d9d71fa8f36
Andrew Pavlyuk <dedice50@gmail.com>
2016-08-30 19:21:07 +0300

    create two components for course managment

---
+++ source/components/common/text.input.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+export default class TextInput extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    render() {
+        return (
+            <input />
+        );
+    }
+}
--- source/components/course/course.form.jsx
+++ source/components/course/course.form.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import TextInput from '../common/text.input';
+import SelectInput from '../common/select.input';
 
-const CourseForm = () => {
+const CourseForm = (props) => {
     return (
-        <h1>Manage Course Form</h1>
+        <form>
+            <h1>Manage Course Form</h1>
+            <TextInput />
+            <SelectInput />
+            <TextInput />
+            <TextInput />
+            <input
+                type="submit"
+                disabled={props.loading}
+                value={props.loading ? 'Saving...' : 'Save'}
+                className="btn btn-primary"
+                onClick={props.onSave} />
+        </form>
     );
 };
 
